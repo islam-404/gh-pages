2c51fd02f0557544fac41e1fe7afd460f225110f
islam <islamkompaev@mail.ru>
2020-01-17 05:19:33 +0300

    урок первый гото

--- 1/js.js
+++ 1/js.js
@@ -9,13 +9,19 @@ let mainList = {
     open: false
 }
 
-for (let i = 0; i < 5; i++) {
-
+for (let i = 0; i < 2; i++) {
+    
     let a = prompt(" Какой тип товаров будем продавать? ");
-    mainList.shopGoods[i] = a;
-
+    innerloop:
+    if ((typeof(a)) === "string" && (typeof(a)) === null && a != "" && a.length < 50) {
+        console.log(" Все верно! ");
+        mainList.shopGoods[i] = a;
+    } else {
+        prompt(" введите корректное название ");
+        break innerloop;
+    }
 }
 
-alert(mainList.budget / 30);
+//alert(mainList.budget / 30);
 
 console.log(mainList);
